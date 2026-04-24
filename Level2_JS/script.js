d9fdba73e76179606af16706be913c2cf498ea65
Dhanush <dhanushcp157@gmail.com>
2026-04-24 10:53:24 +0530

    Added Level 2 JS tasks

---
+++ Level2_JS/script.js
@@ -0,0 +1,16 @@
+// Task 1: Variables (var, let, const)
+let studentName = "Dhanush";
+const college = "Don Bosco College";
+let age = 20;
+
+// Task 2: Data Types
+let isLearningJS = true; // Boolean
+let skills = ["HTML", "CSS", "JS"]; // Array
+
+// Output to Console
+console.log("Name:", studentName);
+console.log("College:", college);
+console.log("Age:", age);
+console.log("Learning JS:", isLearningJS);
+console.log("Skills:", skills);
+
