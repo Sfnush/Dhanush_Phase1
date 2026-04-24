46a2dd99722bab43694f7ffe7ea75e4aad81a8cb
Dhanush <dhanushcp157@gmail.com>
2026-04-24 10:59:42 +0530

    Added Functions and Arrays task

--- Level2_JS/script.js
+++ Level2_JS/script.js
@@ -14,3 +14,24 @@ console.log("Age:", age);
 console.log("Learning JS:", isLearningJS);
 console.log("Skills:", skills);
 
+// Task 2.1: Even/Odd Checker Function
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        return number + " is Even";
+    } else {
+        return number + " is Odd";
+    }
+}
+
+// Testing the function
+console.log(checkEvenOrOdd(10)); // Output: 10 is Even
+console.log(checkEvenOrOdd(7));  // Output: 7 is Odd
+
+// Task 2.2: Arrays & Displaying Items
+let myHobbyList = ["Music Production", "Photography", "Coding", "Biking"];
+
+console.log("My Hobbies:");
+myHobbyList.forEach(function(hobby) {
+    console.log("- " + hobby);
+});
+
